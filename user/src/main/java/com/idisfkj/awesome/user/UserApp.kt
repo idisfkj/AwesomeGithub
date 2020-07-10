a90e59f67acb7d8a25448f925119eae53507f6b9
idisfkj <idisfkj@gmail.com>
2020-07-10 17:12:22 +0800

    fix: 修复user模块独立开发时followers与following默认桥接未注入

--- user/src/main/java/com/idisfkj/awesome/user/UserApp.kt
+++ user/src/main/java/com/idisfkj/awesome/user/UserApp.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import com.alibaba.android.arouter.launcher.ARouter
 import com.idisfkj.awesome.common.utils.SPUtils
 import com.idisfkj.awesome.componentbridge.app.DefaultAppBridge
+import com.idisfkj.awesome.componentbridge.followers.DefaultFollowersBridge
+import com.idisfkj.awesome.componentbridge.following.DefaultFollowingBridge
 import com.idisfkj.awesome.componentbridge.provider.BridgeProviders
 import com.idisfkj.awesome.componentbridge.repos.DefaultReposBridge
 import timber.log.Timber
@@ -23,6 +25,8 @@ class UserApp : Application() {
         BridgeProviders.instance
             .register(DefaultAppBridge::class.java)
             .register(DefaultReposBridge::class.java)
+            .register(DefaultFollowersBridge::class.java)
+            .register(DefaultFollowingBridge::class.java)
     }
 
     private fun initTimber() {
